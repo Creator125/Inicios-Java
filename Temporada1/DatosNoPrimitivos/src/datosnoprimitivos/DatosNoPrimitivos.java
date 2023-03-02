c2379d76a70b68ee107afd8d3fb52d8ee4e652a3
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-03-01 20:23:14 -0500

    Temporada 1/Tema: Basico de Java

---
+++ Temporada1/DatosNoPrimitivos/src/datosnoprimitivos/DatosNoPrimitivos.java
@@ -0,0 +1,19 @@
+/*
+Tema: Datos no primitivos
+ */
+package datosnoprimitivos;
+
+public class DatosNoPrimitivos {
+
+    public static void main(String[] args) {
+        //Dato de tipo objeto
+        Integer num1 = null;
+        //Dato de tipo String
+        String text = "Hola mi yeco";
+        
+        //Imprimiedo datos
+        System.out.println(num1);
+        System.out.println(text);
+    }
+    
+}
---
+++ Temporada1/TipoDatos/src/tipodatos/TipoDatos.java
@@ -0,0 +1,37 @@
+/*
+Tema: Tipo de datos
+*/
+package tipodatos;
+
+public class TipoDatos {
+
+    public static void main(String[] args) {
+        //Variables tipo entera
+        byte num1 = 10;
+        short num2 = 1234;
+        int num3 = 25345;
+        long num4 = 2635274;
+        //Variables de tipo decimal
+        float decil1 = 3.56f;
+        double decil2 =4.87;
+        //Variable de tipo caracter
+        char caracter = 'A';
+        //Variable de tipo booleano
+        boolean desicion = true;
+        
+        //Mostrando datos
+        //Enteros
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        //Decimales
+        System.out.println(decil1);
+        System.out.println(decil2);
+        //Caracter
+        System.out.println(caracter);
+        //Booleano
+        System.out.println(desicion);
+    }
+    
+}
